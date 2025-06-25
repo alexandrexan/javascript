e4681490cdebf10eda63278a06ff4973b5b02f0d
alexandrexan <xampsbeatz@gmail.com>
2025-06-25 20:05:57 -0300

    Condições JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velo = 100
+console.log(`A velocidade do seu carro é ${velo} km/h`)
+if (velo > 80) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Use sempre o cinto de segurança!')
