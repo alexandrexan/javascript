4e9aa5ed27b5c973ce72e853108b8c6878ee5729
alexandrexan <xampsbeatz@gmail.com>
2025-06-30 19:54:01 -0300

    ex011-012-013
    js

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var vota = 33
+console.log(`Você tem ${vota} anos.`)
+if (vota < 16) {
+    console.log('Não vota')
+} else if (vota < 18 || vota > 70) {
+    console.log('Voto opcional')
+    } else {
+        console.log('Voto obrigatório')
+    }
