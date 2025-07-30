0793e643815d1742a41f36b6ba0770ecb02525fe
alexandrexan <xampsbeatz@gmail.com>
2025-07-30 20:14:51 -0300

    Create teste-for.js

---
+++ aula14/teste-for.js
@@ -0,0 +1,14 @@
+/* 
+var c = 1
+while (c <= 5) {
+    console.log(`Contagem: ${c}`)
+    c++
+}
+*/
+
+
+console.log('Salve');
+for (var c=1; c <= 5; c++) {
+    console.log(c)
+} 
+console.log('Fim da contagem');
