f9bb7533896035036d23997b1915b6ce8e59f76d
alexandrexan <xampsbeatz@gmail.com>
2025-07-30 19:59:58 -0300

    Create testes-while-do.js

---
+++ aula13/testes-while-do.js
@@ -0,0 +1,23 @@
+var conta = 1;
+do {
+    console.log(`Contando ${conta}`);
+    conta++;
+} while (conta <= 10);
+
+
+/*
+var conta = 1;
+while (conta <= 10) {
+    console.log(`Contando ${conta}`);
+    conta++; // Incrementa a variável conta em 1 a cada iteração - c = c + 1
+}
+*/
+
+
+/*
+console.log('Salve');
+console.log('Salve');
+console.log('Salve');
+console.log('Salve');
+console.log('Salve');
+*/
